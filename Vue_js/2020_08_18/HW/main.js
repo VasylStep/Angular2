3353c87b35ec4750608e3fd27dd671333ee59f4c
Vasyl_user <vertyporokh.vasyl@gmail.com>
2020-09-10 22:34:59 +0300

    save_All

--- Vue_js/2020_08_18/HW/main.js
+++ Vue_js/2020_08_18/HW/main.js
@@ -1,12 +1,50 @@
-new Vue ({
+// new Vue ({
+//    el: '#app',
+//    data: {
+//       title: "Hello World",
+//       styleCSS:''
+//    },
+//    methods:{
+//       changeText(){
+//          this.title = 'noviy text'
+//       }
+//    }
+// });
+
+// new Vue ({
+//    el:'#app',
+//    data:{
+//       value:1,
+
+//    },
+//    methods:{
+//       increment(value){
+//          this.value=value
+//          if (value==25)
+//          alert ("Are you shure?")
+
+//       }
+//    },
+//    computed: {
+//       doubleValue (){
+//          return this.value*2
+//       }
+
+//    }
+// })
+
+new Vue({
    el: '#app',
    data: {
-      title: "Hello World",
-      styleCSS:''
+      show: false,
+      cars: [
+         { model: "BMW", speed: 250.8 },
+         { model: "Audi", speed: 240.21 },
+         { model: "Mers", speed: 350.4 },
+         { model: "Ford", speed: 160.5 }
+      ]
    },
-   methods:{
-      changeText(){
-         this.title = 'noviy text'
-      }
+   methods: {
+
    }
-});
+})
